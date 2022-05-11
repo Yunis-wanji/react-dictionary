1f50bf4998836c7aae2384cce74ab5054f8fe022
Yunis-wanji <94746081+Yunis-wanji@users.noreply.github.com>
2022-05-11 13:03:12 +0300

    imported bootsrap

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <img src='/images/lens.jpg' alt='lens' className='lens image-fluid'/>
       </header>
     </div>
   );
